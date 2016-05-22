4d586c72b2c776280fd978aa5b73c5817878b58c
solvire <stevenjscott@gmail.com>
2016-05-21 21:59:13 -0700

    tests

---
+++ tests/test_epos-xml.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_epos-xml
+----------------------------------
+
+Tests for `epos-xml` module.
+"""
+
+import pytest
+
+
+from epos-xml import epos-xml
+
+class TestEpos-Xml(object):
+
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    def test_something(self):
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        pass
+
